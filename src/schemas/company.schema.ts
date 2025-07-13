8ff6758a0454eda5402d74ff731d36fa7e761b8a
Kaushith_Gowda_M <koushith7@gmail.com>
2025-07-13 17:46:35 +0530

    schema update

---
+++ src/schemas/company.schema.ts
@@ -0,0 +1,83 @@
+import { z } from 'zod';
+
+export const companySchema = z.object({
+  name: z
+    .string()
+    .nonempty('Company name is required')
+    .min(2, 'Too short')
+    .max(40, 'Too long'),
+
+  establishedOn: z
+    .string()
+    .nonempty('Established date is required'),
+
+  website: z
+    .string()
+    .url('Invalid website URL')
+    .min(10, 'Too short')
+    .max(30, 'Too long')
+    .default('https://'),
+
+  registrationNumber: z
+    .string()
+    .nonempty('Registration number is required')
+    .min(2, 'Too short')
+    .max(20, 'Too long'),
+
+  address1: z
+    .string()
+    .nonempty('Address1 is required')
+    .min(2, 'Too short')
+    .max(50, 'Too long'),
+
+  address2: z
+    .string()
+    .optional(),
+
+  country: z
+    .string()
+    .nonempty('Country is required')
+    .min(2, 'Too short')
+    .max(20, 'Too long'),
+
+  city: z
+    .string()
+    .nonempty('City is required')
+    .min(2, 'Too short')
+    .max(20, 'Too long'),
+
+  state: z
+    .string()
+    .nonempty('State is required')
+    .min(2, 'Too short')
+    .max(20, 'Too long'),
+
+  zipCode: z
+    .string()
+    .nonempty('Zip Code is required'),
+
+  contactFirstName: z
+    .string()
+    .nonempty('First name is required')
+    .min(2, 'Too short')
+    .max(20, 'Too long'),
+
+  contactLastName: z
+    .string()
+    .nonempty('Last name is required')
+    .min(2, 'Too short')
+    .max(20, 'Too long'),
+
+  contactEmail: z
+    .string()
+    .nonempty('Email is required')
+    .email('Invalid email')
+    .min(5, 'Email too short')
+    .max(20, 'Email too long'),
+
+  contactMobile: z
+    .string()
+    .nonempty('Mobile number is required')
+    .min(10, 'Invalid mobile number')
+    .max(10, 'Invalid mobile number')
+});
---
+++ src/schemas/drivers.schema.ts
@@ -0,0 +1,65 @@
+const { z } = require("zod");
+
+export const driverSchema = z.object({
+  firstName: z
+    .string()
+    .min(2, 'First name is required')
+    .max(20, 'Too long'),
+
+  lastName: z
+    .string()
+    .min(2, 'Last name is required')
+    .max(20, 'Too long'),
+
+  email: z
+    .string()
+    .email('Invalid email')
+    .min(5, 'Email too short')
+    .max(20, 'Email too long'),
+
+  mobile: z
+    .string()
+    .min(10, 'Invalid mobile number')
+    .max(10, 'Invalid mobile number'),
+
+  dateOfBirth: z
+    .string()
+    .min(1, 'Date of birth is required'),
+
+  licenseNumber: z
+    .string()
+    .min(2, 'License number is required')
+    .max(20, 'Too long'),
+
+  experience: z
+    .string()
+    .min(1, 'Experience is required'),
+
+  address1: z
+    .string()
+    .min(2, 'Address1 is required')
+    .max(50, 'Too long'),
+
+  address2: z
+    .string()
+    .optional(),
+
+  country: z
+    .string()
+    .min(2, 'Country is required')
+    .max(20, 'Too long'),
+
+  city: z
+    .string()
+    .min(2, 'City is required')
+    .max(20, 'Too long'),
+
+  state: z
+    .string()
+    .min(2, 'State is required')
+    .max(20, 'Too long'),
+
+  zipCode: z
+    .string()
+    .min(1, 'Zip Code is required'),
+});
